d5d5ac2ff57511169d544aba2df6215185d18130
JoelFx <G00408551@atu.ie>
2024-09-29 19:30:44 +0000

    3b complete

--- 3.js
+++ 3.js
@@ -5,7 +5,20 @@ let Tasks = [];
 let addTask = (task) =>{
     if(Tasks.indexOf(task)== -1){
     Tasks.push(task);
-    console.log(task + "has been added to my Tasks");
-    return Tasks.length
+    console.log(task + " has been added to my Tasks");
+    return Tasks.length;
+    }else{
+        console.log(task + " has already been added to my tasks" );
     }
 }
+addTask("Badminton");
+addTask("Football");
+addTask("Tennis");
+addTask("Basketball");
+addTask("Cricket");
+addTask("Baseball");
+addTask("Hurling");
+addTask("Rugby");
+console.log("\n");
+
+
